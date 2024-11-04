fef980e73adfe800bd9c1e6ed2d1dc379d0f0c12
Paulramirezz <ramirezjohnpaul747@gmail.com>
2024-11-04 19:30:08 +0800

    Create userProfile.js

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let fullName = "John Paul T. Ramirez";
+let age = 19;
+let favoriteNumber = 09;
+let favoriteColor = "blue";
+
+console.log(fullName);
+console.log(age);
+console.log(favoriteNumber);
+console.log(favoriteColor);
